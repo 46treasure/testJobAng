399ffa3e01d6da8f31f41d6169386db221a28e05
46treasure <45ytrasure@gmail.com>
2019-07-27 19:36:08 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormArray, FormControl, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jobtestFront';
+export class AppComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  imagesForDownload: string[] = [];
+  image = '';
+  // inputForm: FormGroup;
+  // inputs: Input[];
+
+  ngOnInit(): void {
+
+
+    // this.inputForm = this.fb.group({
+    //   inputs: this.fb.array([this.fb.group({url: ''})])
+    // });
+  }
+
+
+  // get inputPoints() {
+  //   return this.inputForm.get('inputs') as FormArray;
+  // }
+  //
+  // addInput() {
+  //   this.inputPoints.push(this.fb.group({url: ''}));
+  // }
+
+  // deleteInput(index) {
+  //   this.inputPoints.removeAt(index);
+  // }
+
+  getUrl($event) {
+    this.image = $event.target.value;
+    this.imagesForDownload.push(this.image);
+    (document.querySelector('.bg-modal') as HTMLElement).style.display = 'flex';
+  }
+
+
+  download() {
+  }
+
+  close() {
+    (document.querySelector('.bg-modal') as HTMLElement).style.display = 'none';
+  }
+
+
+  sendForm(form: NgForm) {
+    console.log(this.imagesForDownload);
+    console.log(form.value);
+  }
 }
+
+
+
+
